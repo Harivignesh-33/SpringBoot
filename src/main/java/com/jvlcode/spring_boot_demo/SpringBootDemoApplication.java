4e09356dbe7fb89299dba200b111fc77861a0092
Harivignesh-33 <harivigneshhari61@gmail.com>
2025-09-09 07:15:02 +0530

    comment added

--- src/main/java/com/jvlcode/spring_boot_demo/SpringBootDemoApplication.java
+++ src/main/java/com/jvlcode/spring_boot_demo/SpringBootDemoApplication.java
@@ -2,7 +2,7 @@ package com.jvlcode.spring_boot_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//? comments added
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
